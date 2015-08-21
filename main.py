b8ce9e8f21f66685147c331069980f48b5fca990
Rakholiya Jenish <rakholiyajenish.07@gmail.com>
2015-08-21 15:42:56 +0530

    Added username and way to output to file.

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import argparse
 import socket
 import sys
+import os
 
 parser = argparse.ArgumentParser(description="My simple IRC logger")
 parser.add_argument("-s","--network",help="Enter the name of the IRC network to join",metavar='')
 parser.add_argument("-p","--port",type=int,help="Enter the port to connect",metavar='')
 parser.add_argument("-c","--channel",help="Enter the IRC channel to join on the server",metavar='')
 parser.add_argument("-n","--nick",help="Enter the nick to use",metavar='')
+parser.add_argument("-u","--user",help="Enter the username",metavar='')
 parser.add_argument("-o","--output",help="Enter the filename to write the chat log", metavar='')
 args = parser.parse_args()
 
@@ -26,6 +28,24 @@ if args.nick:
 	nick = args.nick
 else:
 	nick = raw_input("Enter the nick to use: ")
+if args.user:
+	user = args.user
+else:
+	temp = raw_input("Enter the username to use: ")
+	if temp:
+		user = temp
+	else:
+		user = 'GOD'
+flag = 0
+if args.output:
+	filename = args.output
+	flag = 1
+	if os.path.isfile(filename):
+		print "Warning: " + filename + " already exist. Enter 'y' or 'n' to append to a file or overwrite existing file."
+	if raw_input() == 'y':
+		out_file = open(filename,'a')
+	else:
+		out_file = open(filename,'w')
 
 try:
 	irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -40,12 +60,22 @@ except socket.error, msg:
 	print 'Failed to connect to the network. Error code: ' + str(msg[0]) + ', Error message: ' + msg[1]
 
 print irc.recv(4096)
-irc.send('NICK '+nick+'\r\n')
-irc.send('USER hello hello hello : GOD IRC\r\n')
+irc.send('NICK ' + nick + '\r\n')
+irc.send('USER ' + user + ' ' + user + ' ' + user + ' : ' + user + '\r\n')
 irc.send('JOIN ' + channel + '\r\n')
 
 while True:
 	data = irc.recv(4096)
-	print "<" + data.split(':')[1].split('!')[0] + "> " + ":".join((data.split(':'))[2:]),
 	if data.find('PING') != -1:
 		irc.send('PONG ' + data.split()[1] + '\r\n')
+	elif data.find('PRIVMSG') != -1:
+		destination = ''.join(data.split(':')[:2]).split(' ')[-2]
+		if destination == nick:
+			destination = '~~/PRIVATE/~~'
+		else:
+			destination = ''
+		str_to_print = destination + "<" + data.split('!')[0].replace(':','') + "> " + ":".join((data.split(':'))[2:])
+		if flag == 0:
+			print str_to_print,
+		else:
+			out_file.write(str_to_print)
